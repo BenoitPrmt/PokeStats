3ad6e17b5e0eb35841610fc5d4f44ba443e9f5e1
BenoitPrmt <benoitparmentier45@gmail.com>
2024-01-08 14:09:52 +0100

    adding pokemon list

--- script.js
+++ script.js
@@ -29,21 +29,55 @@ function showGenerations(max) {
 }
 
 function getGeneration(gen) {
-    const apiCall = fetch("https://pokebuildapi.fr/api/v1/pokemon/generation/"+gen, {
+
+    let pokemonsSection = document.getElementById("pokemons")
+    pokemonsSection.innerHTML = ""
+
+    let loadingEle = document.createElement("h4");
+    loadingEle.innerHTML = "Chargement...";
+    loadingEle.classList.add("loading-text");
+
+    pokemonsSection.appendChild(loadingEle);
+
+    const apiCall = fetch("https://pokebuildapi.fr/api/v1/pokemon/generation/" + gen, {
         method: "GET",
         headers: headers,
     }).then((response) => response.json())
         .then((response) => {
             console.log(response);
-            let pokemonsSection = document.getElementById("pokemons")
             pokemonsSection.innerHTML = ""
             response.forEach(poke => {
-                let pokeEle = document.createElement("p")
-                pokeEle.innerHTML = poke.name
+                let pokeEle = document.createElement("div");
+                pokeEle.classList.add("card")
+                pokeEle.setAttribute("onclick", "getPokemon(" + poke.pokedexId + ")")
+                // pokeEle.setAttribute("href", window.location.href + "pokemon/" + poke.pokedexId)
+
+                let pokeName = document.createElement("h3");
+                pokeName.innerHTML = poke.name
+
+                let pokeImage = document.createElement("img");
+                pokeImage.setAttribute("src", poke.image)
+
+                pokeEle.appendChild(pokeName)
+                pokeEle.appendChild(pokeImage)
                 pokemonsSection.appendChild(pokeEle)
             });
         })
         .catch((error) => { error });
 }
 
-showGenerations(5)
+function getPokemon(poke_id) {
+
+    console.log(window.location);
+
+    const apiCall = fetch("https://pokebuildapi.fr/api/v1/pokemon/" + poke_id, {
+        method: "GET",
+        headers: headers,
+    }).then((response) => response.json())
+        .then((response) => {
+            console.log(response);
+        })
+        .catch((error) => { error });
+}
+
+showGenerations(8)
