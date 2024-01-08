df5a3f6c6b88c986f45a9f75bac8849b8b5fcc6a
BenoitPrmt <benoitparmentier45@gmail.com>
2024-01-08 20:11:11 +0100

    added types in local storage

--- API/type.js
+++ API/type.js
@@ -1,37 +1,83 @@
 // get all pokemons of the same type
 
+function fetchTypes() {
+    fetch("https://pokebuildapi.fr/api/v1/types")
+        .then(response => response.json())
+        .then(data => {
+            let types = [];
+            for (let type of data) {
+                types.push({
+                    name: type.name,
+                    image: type.image,
+                });
+            }
+            localStorage.setItem("types", JSON.stringify(types));
+            showTypes();
+        });
+}
+
 function showTypes() {
     let typesEle = document.getElementById("types");
 
-    fetch("https://pokebuildapi.fr/api/v1/types", {
-        headers: headers
-    })
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            console.log(data);
+    let typesSession = localStorage.getItem("types");
 
-            for (let type of data) {
-                let typeEle = document.createElement("a");
-                typeEle.setAttribute("href", "?type=" + type.name);
+    if (typesSession) {
+        typesSession = JSON.parse(typesSession);
 
-                let typeImage = document.createElement("img");
-                typeImage.classList.add("type-little");
-                typeImage.setAttribute("alt", type.name);
-                typeImage.setAttribute("title", type.name);
-                typeImage.setAttribute("src", type.image);
+        for (let type of typesSession) {
 
-                // let typeName = document.createElement("h3");
-                // typeName.innerHTML = type.name;
+            console.log(type);
 
-                typeEle.appendChild(typeImage);
-                // typeEle.appendChild(typeName);
+            let typeEle = document.createElement("a");
+            typeEle.setAttribute("href", "?type=" + type.name);
 
-                typesEle.appendChild(typeEle);
-            }
-        })
-        .catch((error) => {
-            console.log(error);
-        })
+            let typeImage = document.createElement("img");
+            typeImage.classList.add("type-little");
+            typeImage.setAttribute("alt", type.name);
+            typeImage.setAttribute("title", type.name);
+            typeImage.setAttribute("src", type.image);
+
+            // let typeName = document.createElement("h3");
+            // typeName.innerHTML = type.name;
+
+            typeEle.appendChild(typeImage);
+            // typeEle.appendChild(typeName);
+
+            typesEle.appendChild(typeEle);
+        }
+    } else {
+        fetchTypes();
+    }
 }
+
+// fetch("https://pokebuildapi.fr/api/v1/types", {
+//         headers: headers
+//     })
+//         .then((response) => {
+//             return response.json();
+//         })
+//         .then((data) => {
+//             console.log(data);
+
+//             for (let type of data) {
+//                 let typeEle = document.createElement("a");
+//                 typeEle.setAttribute("href", "?type=" + type.name);
+
+//                 let typeImage = document.createElement("img");
+//                 typeImage.classList.add("type-little");
+//                 typeImage.setAttribute("alt", type.name);
+//                 typeImage.setAttribute("title", type.name);
+//                 typeImage.setAttribute("src", type.image);
+
+//                 // let typeName = document.createElement("h3");
+//                 // typeName.innerHTML = type.name;
+
+//                 typeEle.appendChild(typeImage);
+//                 // typeEle.appendChild(typeName);
+
+//                 typesEle.appendChild(typeEle);
+//             }
+//         })
+//         .catch((error) => {
+//             console.log(error);
+//         })
