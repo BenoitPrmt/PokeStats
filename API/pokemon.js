28ff88fe3f9049d840be8d69716175d2ef33a2af
BenoitPrmt <benoitparmentier45@gmail.com>
2024-01-09 12:05:31 +0100

    adding html elements instead of generating i

--- API/pokemon.js
+++ API/pokemon.js
@@ -10,35 +10,18 @@ const STAT_TEXT = {
 
 function showPokemon(poke) {
 
-    let pokemonSection = document.getElementById("pokemon");
-
     // ---- SECTION LEFT ----
-    let sectionLeft = document.createElement("div");
-    sectionLeft.setAttribute("id", "section-left");
-
-    let pokemonImage = document.createElement("img");
-    pokemonImage.setAttribute("id", "poke-image");
+    let pokemonImage = document.getElementById("poke-image");
     pokemonImage.setAttribute("src", poke.sprites.regular);
-
-    sectionLeft.appendChild(pokemonImage);
     // ---- END SECTION LEFT ----
 
 
     // ---- SECTION RIGHT ----
-
-    let sectionRight = document.createElement("div");
-    sectionRight.setAttribute("id", "section-right");
-
-    // -- SECTION HEADER --
-    let sectionHeader = document.createElement("div");
-    sectionHeader.setAttribute("id", "section-header");
-
-    let pokemonTitle = document.createElement("h2");
-    pokemonTitle.setAttribute("id", "poke-name");
+    let pokemonTitle = document.getElementById("poke-name");
     pokemonTitle.innerHTML = poke.name.fr;
 
-    let pokemonTypes = document.createElement("div");
-    pokemonTypes.setAttribute("id", "poke-types");
+    // let pokemonTypes = document.createElement("div");
+    let pokemonTypes= document.getElementById("poke-types");
 
     for (let type in poke.types) {
         // show type image
@@ -54,46 +37,23 @@ function showPokemon(poke) {
         typeEleLink.appendChild(typeEle);
         pokemonTypes.appendChild(typeEleLink);
     }
-
-    sectionHeader.appendChild(pokemonTitle);
-    sectionHeader.appendChild(pokemonTypes);
     // -- END SECTION HEADER --
 
     // GENERATION
-    let generation = document.createElement("div");
-    generation.setAttribute("id", "generation");
-
-    let generationTitle = document.createElement("h3");
+    let generationTitle = document.getElementById("gen-title");
     generationTitle.innerHTML = "Génération " + poke.generation;
-
-    generation.appendChild(generationTitle);
 
 
     // STATS
-    let stats = document.createElement("div");
-    stats.setAttribute("id", "stats");
-
-    let statsTitle = document.createElement("h3");
-    statsTitle.innerHTML = "Stats";
-
-    let statsList = document.createElement("ul");
+    let statsList = document.getElementById("stats-list");
 
     for (let stat in poke.stats) {
         let statEle = document.createElement("li");
+        statEle.setAttribute("id", "stat-"+stat)
         statEle.innerHTML = STAT_TEXT[stat] + " : " + poke.stats[stat];
         statsList.appendChild(statEle);
     }
-
-    stats.appendChild(statsTitle);
-    stats.appendChild(statsList);
-
-    sectionRight.appendChild(sectionHeader);
-    sectionRight.appendChild(generation)
-    sectionRight.appendChild(stats);
     // ---- END SECTION RIGHT ----
-
-    pokemonSection.appendChild(sectionLeft);
-    pokemonSection.appendChild(sectionRight);
 
 }
 
