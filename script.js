f46829bafbf4c9758fc89328aadb15d1225191cf
BenoitPrmt <benoitparmentier45@gmail.com>
2024-01-11 22:35:56 +0100

    fix arrow

--- script.js
+++ script.js
@@ -58,9 +58,9 @@ if (generationId === null) {
 }
 
 function navigatePage(direction) {
-    if (direction === "previous" && pokemonId < 1) {
+    if (direction === "previous" && pokemonId > 1) {
         getPokemonPage(parseInt(pokemonId)-1)
-    } else if (direction === "next" && pokemonId > 1017) {
+    } else if (direction === "next" && pokemonId < 1017) {
         getPokemonPage(parseInt(pokemonId)+1)
     } else {
         getPokemonPage(pokemonId)
