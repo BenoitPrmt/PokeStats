5127e0052a5259035dc458677bb0deed4c15e363
BenoitPrmt <benoitparmentier45@gmail.com>
2024-11-18 16:25:13 +0100

    feat(router): added react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {useParams} from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { id } = useParams();
 
   return (
-  <h1 className="text-center">Pokestats</h1>
+  <h1 className="text-center">Pokestats {id}</h1>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+    },
+    {
+        path: "/gen/:id",
+        element: <App/>,
+    },
+    {
+        path: "/pokemon/:id",
+        element: <App/>,
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
