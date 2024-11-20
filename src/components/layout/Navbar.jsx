094b8491f8f53ec9ad4f52513b03792aefa38334
BenoitPrmt <benoitparmentier45@gmail.com>
2024-11-20 18:23:19 +0100

    feat(api): added api fetch to get all pokemons by gen

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -13,7 +13,7 @@ function Navbar() {
           className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-44 p-2 shadow">
           {Array.from({length: 9}).map((_, idx) => (
             <li key={idx}>
-              <a className="cursor-pointer">
+              <a className="cursor-pointer" href={`/gen/${idx+1}`}>
                 <img
                   src="https://img.icons8.com/?size=100&id=63311&format=png&color=000000"
                   className="size-14"
@@ -28,7 +28,7 @@ function Navbar() {
           <ul className="flex gap-1">
             {Array.from({length: 9}).map((_, idx) => (
               <li key={idx}>
-                <a className="cursor-pointer">
+                <a className="cursor-pointer" href={`/gen/${idx+1}`}>
                   <img
                     src="https://img.icons8.com/?size=100&id=aAtapxcSmd5w&format=png&color=FFFFFF"
                     className="size-14"
