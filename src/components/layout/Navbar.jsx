8ff8f5aab9b0f9680ccfd39143de8fb6ee92758c
KilianOlry <kilian.olry@gmail.com>
2024-11-22 21:03:30 +0100

    /feat/ add image on pokeball in this project

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-
+import pokeballLogo from "../../../src/assets/images/pokeball.webp";
 const Navbar = () => {
 
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -9,13 +9,12 @@ const Navbar = () => {
     e.preventDefault();
     setIsMenuOpen(!isMenuOpen);
   };
-
   return (
     <header className="flex justify-between items-center py-8 px-3 sm:px-0">
 
       <div role="button" className="lg:hidden" onClick={toggleMenu}>
         <img
-          src="https://img.icons8.com/?size=100&id=63311&format=png&color=000000"
+          src={pokeballLogo}
           className="size-14 anim-pokeBall"
           alt="Burger icon"
         />
@@ -31,7 +30,7 @@ const Navbar = () => {
                 href={`/gen/${idx + 1}`}
               >
                 <img
-                  src="https://img.icons8.com/?size=100&id=63311&format=png&color=000000"
+                  src={pokeballLogo}
                   className="size-16"
                   alt={`Icon ${idx}`}
                 />
@@ -40,7 +39,7 @@ const Navbar = () => {
           ))}
         </ul>
         {isMenuOpen ?
-          <ul className={`flex flex-col gap-1 absolute transform -translate-y-1/2 border top-1/2 left-2 w-52 lg:hidden rounded-3xl glass`}>
+          <ul className={`flex flex-col gap-1 fixed z-50 top-1/2 transform -translate-y-1/2 border left-2 w-52 lg:hidden rounded-3xl glass`}>
             {Array.from({length: 9}).map((_, idx) => (
               <li key={idx} className="rounde-3xl hover:scale-105">
                 <a
@@ -48,7 +47,7 @@ const Navbar = () => {
                   href={`/gen/${idx + 1}`}
                 >
                   <img
-                    src="https://img.icons8.com/?size=100&id=63311&format=png&color=000000"
+                    src={pokeballLogo}
                     className="size-16"
                     alt={`Icon ${idx}`}
                   />
