2f97c48dd1ce6248d5f83a006207c6634d83311b
BenoitPrmt <benoitparmentier45@gmail.com>
2024-11-23 14:43:54 +0100

    fix(pokemonCard): fixed gradient issue and added normalize to remove accent in type name to find the correct color

--- src/components/card-pokemon.jsx
+++ src/components/card-pokemon.jsx
@@ -7,9 +7,9 @@ const cardPokemon = ({pokemon}) => {
       key={pokemon.pokedex_id}
       className="text-center shadow relative h-52 rounded-xl group">
       <div
-        className={`bg-gradient-to-r opacity-30 absolute h-full w-full rounded-xl
+        className={`bg-gradient-to-t opacity-30 absolute h-full w-full rounded-xl
           ${pokemonTypes.some((item) => item.type === pokemon.types[0].name)
-          ? `from-${pokemon.types[0].name.toLowerCase() + "Primaire"} via-${pokemon.types[0].name.toLowerCase() + "Secondaire"}`
+          ? `from-${pokemon.types[0].name.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "") + "Primaire"} via-${pokemon.types[0].name.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "") + "Secondaire"}`
           : "bg-blue-700"}`}>
       </div>
 
@@ -18,7 +18,7 @@ const cardPokemon = ({pokemon}) => {
         alt=""
         className="absolute size-44 z-10 -top-10 transform -translate-x-1/2 -translate-y-1/2 rounded-full left-1/2 group-hover:scale-110 group-hover:-translate-y-2/3 transition duration-300"/>
       <div className="grid place-content-center h-full">
-        <h2 className="my-5 capitalize tracking-wide z-20 text-xl font-medium text-white">{pokemon.name.fr}</h2>
+        <h2 className="my-5 capitalize tracking-wide z-20 text-xl font-medium text-black">{pokemon.name.fr}</h2>
         <a
           href={`/pokemon/${pokemon.pokedex_id}`}
           className="group relative overflow-hidden rounded bg-gray-700 px-10 py-2 text-lg transition-all">
