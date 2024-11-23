396cd37d47dcd95be9a3a53a650e21291bfa2f4d
BenoitPrmt <benoitparmentier45@gmail.com>
2024-11-23 14:44:57 +0100

    feat(Gen): edited title and title size

--- src/pages/Gen.jsx
+++ src/pages/Gen.jsx
@@ -39,7 +39,7 @@ function Gen() {
 
   return (
     <main className="py-20">
-      <Title content={"Bienvenue à la " + id + "ème génération"}/>
+      <Title content={id + (parseInt(id) === 1 ? "ère" : "ème") + " génération de Pokémons"} size={"small"} />
 
       <div className="grid grid-cols-1 sm:grid-cols-2 px-8 lg:px-0 lg:grid-cols-4 gap-x-24 gap-y-48 py-52">
         {pokemons.length > 0 ? (
