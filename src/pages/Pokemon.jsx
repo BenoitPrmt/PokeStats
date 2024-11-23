f93f2a354696170d943148904ec24826657ca133
KilianOlry <kilian.olry@gmail.com>
2024-11-24 00:21:49 +0100

    /feat/ work on responsive of details page

--- src/pages/Pokemon.jsx
+++ src/pages/Pokemon.jsx
@@ -39,9 +39,9 @@ function Pokemon() {
 
   return (
     <main className="">
-      <div className="grid p-4 grid-cols-2  min-h-[calc(100dvh-144px)]">
-        <div className="pl-20">
-          <div className="flex items-center gap-10 justify-between">
+      <div className="grid grid-cols-1 p-4 md:grid-cols-2  min-h-[calc(100dvh-144px)]">
+        <div className="md:pl-20 order-2 md:order-1">
+          <div className="flex flex-col lg:flex-row items-center gap-10 justify-between">
             <h1
               className="text-6xl font-semibold text-gray-900 tracking-tight duration-1000"
               onMouseEnter={(e) => (e.currentTarget.innerHTML = pokemon.name.jp)}
@@ -51,7 +51,7 @@ function Pokemon() {
             </h1>
             <div className="flex justify-center gap-3">
               {pokemon.types.map((type) => (
-                <img key={type.name} src={type.image} alt={type.name} className="size-14 rounded-full"/>
+                <img key={type.name} src={type.image} alt={type.name} className=" size-10 md:size-14 rounded-full"/>
               ))}
             </div>
 
@@ -102,12 +102,13 @@ function Pokemon() {
 
 
         </div>
-        <div className="relative">
-          <div className="bg-red-500 right w-auto h-full right"></div>
+
+        <div className="relative order-1 md:order-2">
+          <div className="bg-red-500 right w-auto h-[400px] md:h-full right"></div>
           <img
             src={pokemon.sprites.regular}
             alt={pokemon.name.fr}
-            className="size-[450px] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+            className="size-80 md:size-[450px] absolute  top-40 md:top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
             onMouseEnter={(e) => {
               if (pokemon.sprites.shiny) {
                 e.currentTarget.src = pokemon.sprites.shiny;
