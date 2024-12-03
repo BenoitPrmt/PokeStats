8c8209fb614f347be30231e05f800e0d3976f706
KilianOlry <kilian.olry@gmail.com>
2024-12-03 22:29:18 +0100

    /feature/ Add condition for display RedirectToHome button on all pages except homepage

--- src/components/layout/nav/navbar.jsx
+++ src/components/layout/nav/navbar.jsx
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import pokeballLogo from "../../../assets/images/pokeball.webp";
 import NavLinkSm from "./navLinkSm.jsx";
 import NavLinkLg from "./navLinkLg.jsx";
+
 const Navbar = () => {
 
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -49,15 +50,19 @@ const Navbar = () => {
       </nav>
 
       <div className="flex gap-3">
-        <a href="/" className="bg-gray-900 flex justify-center items-center rounded-full p-4 text-white">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
-               stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-               className="size-6 lucide lucide-house">
-            <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"/>
-            <path
-              d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/>
-          </svg>
-        </a>
+        {window.location.pathname != "/"
+          ?
+          <a href="/" className="bg-gray-900 flex justify-center items-center rounded-full p-4 text-white">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
+                 stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+                 className="size-6 lucide lucide-house">
+              <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"/>
+              <path
+                d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/>
+            </svg>
+          </a>
+          : ""}
+
         <a href="/random" className="bg-gray-900 flex justify-center items-center rounded-full p-4 text-white">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
                stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
