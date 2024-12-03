05fcf1e542b091119ff3dfadd991ff0e8141d473
KilianOlry <kilian.olry@gmail.com>
2024-12-03 22:21:41 +0100

    /feat/ Add icon for redirect to homepage

--- src/components/layout/nav/navbar.jsx
+++ src/components/layout/nav/navbar.jsx
@@ -48,13 +48,24 @@ const Navbar = () => {
           </ul> : ''}
       </nav>
 
-      <div>
-        <a href="/random">
-          <img
-            src="https://img.icons8.com/?size=100&id=2908&format=png&color=000000"
-            className="size-14"
-            alt="Logo"
-          />
+      <div className="flex gap-3">
+        <a href="/" className="bg-gray-900 flex justify-center items-center rounded-full p-4 text-white">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
+               stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+               className="size-6 lucide lucide-house">
+            <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"/>
+            <path
+              d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/>
+          </svg>
+        </a>
+        <a href="/random" className="bg-gray-900 flex justify-center items-center rounded-full p-4 text-white">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
+               stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+               className="size-6 lucide lucide-circle-help">
+            <circle cx="12" cy="12" r="10"/>
+            <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"/>
+            <path d="M12 17h.01"/>
+          </svg>
         </a>
       </div>
     </header>
