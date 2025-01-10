9c5afd07c4298843c3d5bbbbc46b57a0bd64caa5
BenoitPrmt <benoitparmentier45@gmail.com>
2025-01-10 09:06:29 +0100

    fix(evolution): fixed next evol. length bug

--- src/components/details/carousel.jsx
+++ src/components/details/carousel.jsx
@@ -1,37 +1,41 @@
 const Carousel = ({pokemon}) => {
     let evolution = [];
 
-  if (pokemon.evolution.next.length > 0) {
-    evolution = pokemon.evolution.next.flat();
+    if (pokemon.evolution.pre?.length > 0) {
+        evolution = evolution.concat(pokemon.evolution.pre.flat());
+    }
 
-  }
+    if (pokemon.evolution.next?.length > 0) {
+        evolution = evolution.concat(pokemon.evolution.next.flat());
+    }
 
-  return (
-    <div>
-      <div className="carousel w-full h-52">
-        {evolution.length > 0 ? (
-          evolution.map((item, index) => (
-            <div key={index} id={`item${index + 1}`} className="carousel-item w-full flex justify-center">
-              <img
-                src={`https://raw.githubusercontent.com/Yarkis01/TyraDex/images/sprites/${item.pokedex_id}/regular.png`}
-                alt={`Evolution ${item.pokedex_id}`}
-              />
+    return (
+        <div>
+            <div className="carousel w-full h-52">
+                {evolution.length > 0 ? (
+                    evolution.map((item, index) => (
+                        <a key={index} id={`item${index + 1}`} className="carousel-item w-full flex justify-center"
+                           href={`/pokemon/${item.pokedex_id}`}>
+                            <img
+                                src={`https://raw.githubusercontent.com/Yarkis01/TyraDex/images/sprites/${item.pokedex_id}/regular.png`}
+                                alt={`Evolution ${item.pokedex_id}`}
+                            />
+                        </a>
+                    ))
+                ) : (
+                    ''
+                )}
             </div>
-          ))
-        ) : (
-          ''
-        )}
-      </div>
 
-      <div className="flex w-full justify-center gap-2 py-2">
-        {evolution.map((_, index) => (
-          <a key={index} href={`#item${index + 1}`} className="btn btn-xs">
-            {index + 1}
-          </a>
-        ))}
-      </div>
-    </div>
-  )
+            <div className="flex w-full justify-center gap-2 py-2">
+                {evolution.map((_, index) => (
+                    <a key={index} href={`#item${index + 1}`} className="btn btn-xs">
+                        {index + 1}
+                    </a>
+                ))}
+            </div>
+        </div>
+    )
 }
 
 export default Carousel;
