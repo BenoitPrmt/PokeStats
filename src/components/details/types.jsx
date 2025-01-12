2974f7ec36ae20177721afdd4bc683db57568e20
BenoitPrmt <benoitparmentier45@gmail.com>
2025-01-12 12:14:24 +0100

    feat(types): added link to view all pokemons of a type

--- src/components/details/types.jsx
+++ src/components/details/types.jsx
@@ -1,10 +1,12 @@
 const Types = ({pokemon}) => {
-  return (
-    pokemon.types.map((type) => (
-      <img key={type.name} src={type.image} alt={type.name} className="size-10 rounded-full
+    return (
+        pokemon.types.map((type) => (
+            <a href={"/type/" + type.name.toLowerCase()} key={type.id}>
+                <img key={type.name} src={type.image} alt={type.name} className="size-10 rounded-full
                 md:size-14"/>
-    ))
-  )
+            </a>
+        ))
+    )
 }
 
 export default Types
