a75dd45a2a5471ca1314404e23268d27d80d7c4f
BenoitPrmt <benoitparmentier45@gmail.com>
2025-02-11 11:35:28 +0100

    fix(types): fixed types name with accents

--- src/components/details/types.jsx
+++ src/components/details/types.jsx
@@ -1,7 +1,7 @@
 const Types = ({pokemon}) => {
     return (
         pokemon.types.map((type) => (
-            <a href={"/type/" + type.name.toLowerCase()} key={type.id}>
+            <a href={"/type/" + type.name.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")} key={type.id}>
                 <img key={type.name} src={type.image} alt={type.name} className="size-10 rounded-full
                 md:size-14"/>
             </a>
